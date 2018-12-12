126ea092ef7b71a698319b01799d5ba106f3fc22
sgenini <sgenini@gmail.com>
2018-12-12 14:21:13 -0800

    Add 07-week/day-3 files

---
+++ 07-week/day-3/14-TwoTables/topSongsCode.js
@@ -0,0 +1,191 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "top_songsDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song",
+        "Search for Hit Song & Hit Album in the same year"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Find songs by artist":
+        artistSearch();
+        break;
+
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+
+      case "Search for Hit Song & Hit Album in the same year":
+        bothSearch();
+        break;
+      }
+    });
+}
+
+function artistSearch() {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?"
+    })
+    .then(function(answer) {
+      var query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+        }
+        runSearch();
+      });
+    });
+}
+
+function multiSearch() {
+  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].artist);
+    }
+    runSearch();
+  });
+}
+
+function rangeSearch() {
+  inquirer
+    .prompt([
+      {
+        name: "start",
+        type: "input",
+        message: "Enter starting position: ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "end",
+        type: "input",
+        message: "Enter ending position: ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+      connection.query(query, [answer.start, answer.end], function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "Position: " +
+              res[i].position +
+              " || Song: " +
+              res[i].song +
+              " || Artist: " +
+              res[i].artist +
+              " || Year: " +
+              res[i].year
+          );
+        }
+        runSearch();
+      });
+    });
+}
+
+function songSearch() {
+  inquirer
+    .prompt({
+      name: "song",
+      type: "input",
+      message: "What song would you like to look for?"
+    })
+    .then(function(answer) {
+      console.log(answer.song);
+      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
+        console.log(
+          "Position: " +
+            res[0].position +
+            " || Song: " +
+            res[0].song +
+            " || Artist: " +
+            res[0].artist +
+            " || Year: " +
+            res[0].year
+        );
+        runSearch();
+      });
+    });
+}
+
+function bothSearch() {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "Which artist would you like to look for?"
+    })
+    .then(function(answer) {
+      // console.log(answer.artist);
+      connection.query("SELECT * FROM top5000 INNER JOIN top_albums ON top5000.artist = top_albums.artist AND top5000.year = top_albums.year WHERE ? AND ? ORDER BY top_albums.year", [{ 'top5000.artist': answer.artist }, {'top_albums.artist': answer.artist }], function(err, res) {
+      // connection.query("SELECT * FROM top5000 INNER JOIN top_albums ON top5000.artist = top_albums.artist AND top5000.year = top_albums.year WHERE ?", { 'top5000.artist': answer.artist }, function(err, res) {
+          // console.log(res)
+          for (var i = 0; i < res.length; i++){
+            console.log(
+              "Album Position: " +
+                res[i].position +
+                " || Song: " +
+                res[i].song +
+                " || Album: " +
+                res[i].album +
+                " || Artist: " +
+                res[i].artist +
+                " || Year: " +
+                res[i].year
+            );
+          }
+          console.log("\n");
+        runSearch();
+      });
+    });
+}
